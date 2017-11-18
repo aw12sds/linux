ae27ab369ab9df902c6954fa71a319dc189963c7
test123 <aw12dwc1@gmail.com>
2017-11-18 19:22:22 +0800

    Update webinstall.py

--- webinstall.py
+++ webinstall.py
@@ -9,7 +9,12 @@ def judge():
     os.system("tar -xzvf latest.tar.gz wordpress")
     os.system("rm -rf latest.tar.gz")
     print("wordpress download complete")
-
+#先建立config,贴上数据库
+#插件设置 
+#if ( !defined('ABSPATH') )
+#define('ABSPATH', dirname(__FILE__) . '/');
+#设置格式 apache用户  ps -ef | grep httpd
+#将wordpress用户改为daemon
 if __name__ == "__main__":
 
     judge()
