07b01a4b70b2d843cc4eb538897a9e261e5fe0ad
test123 <aw12dwc1@gmail.com>
2018-05-29 19:34:51 +0800

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -10,3 +10,8 @@ set "Ymd=%date:~,4%%date:~5,2%%date:~8,2%"
 
 =删除回收站
 echo Y|PowerShell.exe -NoProfile -Command Clear-RecycleBin"
+del /f /s /q %systemdrive%\*.tmp
+del /f /s /q %systemdrive%\*._mp 
+del /f /s /q %systemdrive%\*.log 
+del /f /s /q %systemdrive%\*.old 
+del /f /s /q %windir%\*.bak 
