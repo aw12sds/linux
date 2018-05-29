f8ab52c186bfa2bdda9f17c8005a39c75edaac2a
test123 <aw12dwc1@gmail.com>
2018-05-29 18:53:35 +0800

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,1 +1,11 @@
 
+
+=mysql备份
+@echo off
+set "Ymd=%date:~,4%%date:~5,2%%date:~8,2%"
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --skip-extended-insert -u root --password=root zkxj_erp_new> E:\db_backup\performance_%Ymd%.sql
+@echo on
+
+
+
+=
